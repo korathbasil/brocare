2f62f80666a13ec20da497dde6952caf95a6c8e7
Bazil Korath <korathbasil@live.com>
2022-09-10 19:09:43 +0530

    feat [client]: design splash screen logo

---
+++ client/lib/app/core/colors.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+const clrBlack = Colors.black;
+const clrBlue = Color.fromARGB(255, 13, 71, 180);
--- client/lib/ui/splash/splash_screen.dart
+++ client/lib/ui/splash/splash_screen.dart
@@ -1,3 +1,4 @@
+import 'package:client/app/core/colors.dart';
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatelessWidget {
@@ -8,18 +9,46 @@ class SplashScreen extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
           child: Container(
-        color: Colors.black,
+        color: clrBlack,
         padding: EdgeInsets.all(10),
         child: Column(
           children: const [
             Expanded(
                 child: Center(
-              child: Text("BROCARE"),
+              child: Logo(),
             )),
             BottomText()
           ],
         ),
       )),
+    );
+  }
+}
+
+class Logo extends StatelessWidget {
+  const Logo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          decoration: BoxDecoration(
+              color: clrBlue, borderRadius: BorderRadius.circular(10)),
+          margin: const EdgeInsets.only(right: 3),
+          padding: const EdgeInsets.all(3),
+          child: const Text(
+            "BRO",
+            style: TextStyle(
+                fontSize: 40, fontWeight: FontWeight.w900, color: clrBlack),
+          ),
+        ),
+        const Text(
+          "CARE",
+          style: TextStyle(fontSize: 40, fontWeight: FontWeight.w900),
+        )
+      ],
     );
   }
 }
@@ -43,11 +72,11 @@ class BottomText extends StatelessWidget {
           Container(
             decoration: BoxDecoration(
                 color: Colors.white, borderRadius: BorderRadius.circular(5)),
+            margin: const EdgeInsets.only(right: 1),
             padding: const EdgeInsets.fromLTRB(3, 1, 1, 1),
             child: const Text(
               "BRO",
-              style:
-                  TextStyle(fontWeight: FontWeight.w900, color: Colors.black),
+              style: TextStyle(fontWeight: FontWeight.w900, color: clrBlack),
             ),
           ),
           const Text(
