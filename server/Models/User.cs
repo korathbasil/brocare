e67a3460b8feff5a5aef1ec02a4b9cf1888e54a2
Bazil Korath <korathbasil@live.com>
2022-09-14 01:24:32 +0530

    fix [server]: upgrade user model

--- server/Models/User.cs
+++ server/Models/User.cs
@@ -1,14 +1,28 @@
+using System.ComponentModel.DataAnnotations;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
 namespace server.Models;
 
-public class User
+public record User
 {
     [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
     public string? Id { get; set; }
 
+    [Required]
     public string FirstName { get; set; }
+
+    [Required]
     public string LastName { get; set; }
+
+    [Required]
+    public string Email { get; set; }
+
+    [Required]
+    public string Phone { get; set; }
+
+    [Required]
+    public string Password { get; set; }
+
 }
