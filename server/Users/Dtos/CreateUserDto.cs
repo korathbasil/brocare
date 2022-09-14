fcbd039c1ab7e367e049575ae40861234b3ec5cd
Bazil Korath <korathbasil@live.com>
2022-09-14 18:18:48 +0530

    fix [server]: move user dto to users namespace

--- server/Users/Dtos/CreateUserDto.cs
+++ server/Users/Dtos/CreateUserDto.cs
@@ -1,6 +1,6 @@
 using System.ComponentModel.DataAnnotations;
 
-namespace server.Dtos;
+namespace server.Users;
 
 public class CreateUserDto
 {
--- server/Users/UserController.cs
+++ server/Users/UserController.cs
@@ -1,5 +1,4 @@
 using Microsoft.AspNetCore.Mvc;
-using server.Dtos;
 
 namespace server.Users;
 
